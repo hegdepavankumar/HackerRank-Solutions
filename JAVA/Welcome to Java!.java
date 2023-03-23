d95a9f9469598009be428e387f5e7d14965c994e
Pavankumar Hegde <puhegde01@gmail.com>
2023-03-23 11:53:01 +0530

    1file

---
+++ JAVA/Welcome to Java!.java
@@ -0,0 +1,16 @@
+// Author : Pavankumar Hegde
+
+// Question: Write a program to print "Welcome to Java!" on the screen.
+
+// Solution:
+
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
